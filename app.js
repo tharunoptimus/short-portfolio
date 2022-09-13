47cc13ab60f3c9a3816550a92d88aea4d9d28a59
Tharunraj Optimus <tharunoptimus@outlook.com>
2022-09-13 03:21:40 +0000

    Toggle to switch between themes

---
+++ app.js
@@ -0,0 +1,28 @@
+const LOCALSTORAGE_THEME = "theme"
+
+let toggle = document.querySelector(".toggle")
+toggle.addEventListener("click" , toggleTheme)
+
+
+function toggleTheme() {
+    let emoji = getComputedStyle(toggle, ':before').getPropertyValue('content')
+    let theme = emoji == '"🌞"' ? "light" : "dark"
+    setTheme(theme)
+}
+
+function main() {
+    let prefers = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+    let theme = window.localStorage.getItem(LOCALSTORAGE_THEME) ?? prefers
+    setTheme(theme)
+}
+
+function setTheme(theme) {
+    if(theme == "dark") {
+        document.documentElement.classList.add("darkTheme")
+    } else {
+        document.documentElement.classList.remove("darkTheme")
+    }
+    window.localStorage.setItem(LOCALSTORAGE_THEME, theme)
+}
+
+main()
